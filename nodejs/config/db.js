04ec960b8666068bee210ea99a9c689268bc6f1b
DanLevyM <74914471+DanLevyM@users.noreply.github.com>
2022-03-07 23:31:29 +0100

    Delete nodejs directory

--- nodejs/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export default async function connectDB() {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log(`MongoDB Connected ${conn.connection.host}`.blue);
-};
--- nodejs/middleware/logger.js
+++
@@ -1,6 +0,0 @@
-// @desc  Logs request to console
-
-export default function logger(req, res, next) {
-  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-}
