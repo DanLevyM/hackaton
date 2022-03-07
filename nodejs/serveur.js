1abc97bb1a245860e8536f9de0935dcc1579a87a
DanLevyM <dan.levy.pro@gmail.com>
2022-03-07 21:55:32 +0100

    connect db

--- nodejs/serveur.js
+++ nodejs/serveur.js
@@ -1,21 +1,29 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import morgan from 'morgan';
 // Route files
 import adminRoute from './routes/admin.router.js';
-import morgan from 'morgan';
+import connectDB from './config/db.js';
 
+dotenv.config({path: './config/config.env'});
+connectDB();
 const PORT = process.env.PORT || 5001;
 const app = express();
-dotenv.config({path: './config/config.env'});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use('/api/v1/admin', adminRoute);
-app.listen(
+const server = app.listen(
     PORT,
     // eslint-disable-next-line max-len
     console.log(`Serveur running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`),
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.error(`Error: ${err}`);
+  // Close server & exit
+  server.close(() => process.exit(1));
+});
