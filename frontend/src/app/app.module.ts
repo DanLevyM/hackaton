09cceb4c31be6dfb5ef1ff7e6e388d37c396a7de
Júlio César Peixoto Pereira <julioocesarr@outlook.fr>
2022-03-09 00:48:21 +0100

    Implement header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
