83b3c10e0e22e8a670dad602a99fe85371f662cb
Júlio César Peixoto Pereira <julioocesarr@outlook.fr>
2022-03-12 22:45:54 +0100

    Add default dashboards

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+
+import * as echarts from 'echarts'
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +9,156 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild('echarts1', {static: true}) echartsReference1: any;
+  @ViewChild('echarts2', {static: true}) echartsReference2: any;
+  @ViewChild('echarts3', {static: true}) echartsReference3: any;
 
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initChartOne();
+    this.initChartTwo();
+    this.initChartThird();
+  }
+
+  /**
+   * Init first chart
+   */
+  public initChartOne(): void {
+    const myChart = echarts.init(this.echartsReference1.nativeElement);
+    const option = {
+      title: {
+        // text: 'Stacked Line'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Email',
+          type: 'line',
+          stack: 'Total',
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: 'Union Ads',
+          type: 'line',
+          stack: 'Total',
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: 'Video Ads',
+          type: 'line',
+          stack: 'Total',
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: 'Direct',
+          type: 'line',
+          stack: 'Total',
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: 'Search Engine',
+          type: 'line',
+          stack: 'Total',
+          data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+      ]
+    };
+
+    myChart.setOption(option);
+  }
+
+  /**
+   * Init second chart
+   */
+  public initChartTwo(): void {
+    const myChart = echarts.init(this.echartsReference2.nativeElement);
+    const option = {
+      xAxis: {},
+      yAxis: {},
+      series: [
+        {
+          symbolSize: 20,
+          data: [
+            [10.0, 8.04],
+            [8.07, 6.95],
+            [13.0, 7.58],
+            [9.05, 8.81],
+            [11.0, 8.33],
+            [14.0, 7.66],
+            [13.4, 6.81],
+            [10.0, 6.33],
+            [14.0, 8.96],
+            [12.5, 6.82],
+            [9.15, 7.2],
+            [11.5, 7.2],
+            [3.03, 4.23],
+            [12.2, 7.83],
+            [2.02, 4.47],
+            [1.05, 3.33],
+            [4.05, 4.96],
+            [6.03, 7.24],
+            [12.0, 6.26],
+            [12.0, 8.84],
+            [7.08, 5.82],
+            [5.02, 5.68]
+          ],
+          type: 'scatter'
+        }
+      ]
+    };
+
+    myChart.setOption(option);
+  }
+
+  /**
+   * Init third chart
+   */
+  public initChartThird(): void {
+    const myChart = echarts.init(this.echartsReference3.nativeElement);
+    const option = {
+      legend: {},
+      tooltip: {},
+      dataset: {
+        dimensions: ['product', '2015', '2016', '2017'],
+        source: [
+          { product: 'Matcha Latte', 2015: 43.3, 2016: 85.8, 2017: 93.7 },
+          { product: 'Milk Tea', 2015: 83.1, 2016: 73.4, 2017: 55.1 },
+          { product: 'Cheese Cocoa', 2015: 86.4, 2016: 65.2, 2017: 82.5 },
+          { product: 'Walnut Brownie', 2015: 72.4, 2016: 53.9, 2017: 39.1 }
+        ]
+      },
+      xAxis: { type: 'category' },
+      yAxis: {},
+      series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+    };
+
+    myChart.setOption(option);
+  }
 
   /**
    * Logout user
